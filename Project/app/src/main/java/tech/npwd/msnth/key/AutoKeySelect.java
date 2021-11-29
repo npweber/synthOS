ca1ea3bf9b884f00aa89aa31f27efb6551629241
nathanweber068 <nathan.weber068@myci.csuci.edu>
2021-11-28 16:13:01 -0800

    Fixed Bug: MuSynth Max Looping Count

--- Project/app/src/main/java/tech/npwd/msnth/key/AutoKeySelect.java
+++ Project/app/src/main/java/tech/npwd/msnth/key/AutoKeySelect.java
@@ -1,38 +1,42 @@
 package tech.npwd.msnth.key;
 
-import java.util.Random;
+import static tech.npwd.roots.Randomer.random;
 
 import edu.csuci.nw068.snth.generate.ux.GenerationUX;
-
 import tech.npwd.msnth.pattern.Sizes;
 
 public class AutoKeySelect {
 
+    private static final int[] keyNumbersBase = {65, 70, 75, 80, 85};
+    private static final int randomOffsetMax = 5;
+
     private final String keysSelected;
 
-    private final Random random;
-
     public AutoKeySelect(GenerationUX.GenerationUXInput input){
-        this.random = new Random();
         this.keysSelected = selectKeys(input);
     }
 
     private String selectKeys(GenerationUX.GenerationUXInput input){
         StringBuilder selected = new StringBuilder();
 
-        String sizeName = input.getSize().name();
-        int maxNumberOfKeys = Sizes.useSize(sizeName);
-        int numberOfKeys = random.nextInt(maxNumberOfKeys) + 1;
-
-        final int[] keyNumbersBase = {65, 70, 75, 80, 85};
         int keyRangeClass = input.getKeyRange().ordinal();
-        int randMax = 5;
         int keyNumberBase = keyNumbersBase[keyRangeClass];
 
-        int rand;
+        final int numberOfNotes = Sizes.useSize(input.getSize().name());
+        int numberOfKeys = Integer.MAX_VALUE;
+        while(numberOfKeys > numberOfNotes)
+            numberOfKeys = random(-1, randomOffsetMax) + 1;
+
+        int key;
+        boolean duplicate;
         for(int i = 0; i < numberOfKeys; i++) {
-            rand = random.nextInt(randMax);
-            selected.append(keyNumberBase + rand).append(" ");
+            key = keyNumberBase + random(-1, randomOffsetMax);
+            duplicate = selected.toString().contains(String.valueOf(key));
+
+            if(!duplicate)
+                selected.append(key).append(" ");
+            else
+                i--;
         }
 
         return selected.toString();
